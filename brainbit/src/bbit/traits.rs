418f07e538302604f2edd6b4f427a4fe2e66ca1e
Yuriy Larin <blandger@gmail.com>
2025-01-12 13:35:41 +0200

    new GH build script

--- brainbit/src/bbit/traits.rs
+++ brainbit/src/bbit/traits.rs
@@ -8,17 +8,20 @@ pub trait EventHandler {
     /// Dispatched when an internal device status update is received.
     ///
     /// Contains the status, cmd error, battery level.
-    async fn device_status_update(&self, _status: DeviceStatusData) {}
+    async fn device_status_update(&self, _status_data: DeviceStatusData) {}
+    // async fn device_status_update(&self, _status: DeviceStatusData) -> ();
 
     /// Dispatched when an eeg data is received.
     ///
     /// Contains information about the O1, O2, T3, T4 + interval.
     async fn eeg_update(&mut self, _eeg_data: Vec<u8>) {}
+    // async fn eeg_update(&mut self, _eeg_data: Vec<u8>) -> ();
 
     /// Dispatched when measurement data is received over the PMD data UUID.
     ///
     /// Contains data in a [`CommandData`].
     async fn send_command(&self, _command_data: CommandData) {}
+    // async fn send_command(&self, _command_data: CommandData) -> () {        ()     }
 
     /// Checked at start of each event loop.
     ///
